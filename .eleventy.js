3b6858f623c76e76a79e3669247f0e3f0211fa50
bastien1409 <bastien.ravandison@gmail.com>
2025-09-12 08:12:39 -0400

    simplified website closure

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,10 @@
-module.exports = function() {
+module.exports = function(eleventyConfig) {
+  // Tell Eleventy to copy images/static files from src/assets to _site/assets
+  eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+
   return {
     dir: { input: "src", output: "_site", data: "_data" },
     htmlTemplateEngine: "njk"
   };
 };
+
